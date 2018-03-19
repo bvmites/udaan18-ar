cd6fd9d590c4814149d90357127e017dc515d49d
Jp9573 <Jp9573@gmail.com>
2018-03-19 23:12:01 +0530

    first commit

---
+++ Assets/Scripts/EventHandlerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandlerScript : MonoBehaviour {
+	
+	public void showMenu(GameObject panel) {
+		if (panel.activeSelf) {
+			panel.SetActive (false);
+		} else {
+			panel.SetActive (true);		
+		}
+	}
+
+	public void showDevelopedBy() {
+
+	}
+
+	public void showAboutUs() {
+
+	}
+
+}
